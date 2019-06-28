93449a878d82a2027931d7629d8f5211f91de34c
Simmie <skajogbola@users.noreply.github.com>
2019-06-28 13:45:12 -0400

    Updating the Nav folder

--- Client/src/components/Nav/index.js
+++ Client/src/components/Nav/index.js
@@ -1,17 +1,16 @@
 import React from "react";
+import "./style.css";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List 
-      </a>
-      <a className="navbar-brand" href="/">
-        React Reading List 
-      </a>
-      <a className="navbar-brand" href="/">
-        React Reading List 
-      </a>
+      <ul>
+        <li className="navbar-brand">
+          <a href="/">Google Books</a>
+        </li>
+        <li className="brand1">Search</li>
+        <li className="brand2">Saved</li>
+      </ul> 
     </nav>
   );
 }
