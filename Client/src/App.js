131f1b18b14f4d35221f5c0455ece520c6368249
Simmie <skajogbola@users.noreply.github.com>
2019-06-29 07:39:00 -0400

    fixing the app.js file

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import Books from "./pages/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
-    <div>
-    <Nav />
+      <div>
+        <Nav />
         <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
           <Route component={NoMatch} />
-        </Switch> 
-    </div>
+        </Switch>
+      </div>
     </Router>
   );
 }
