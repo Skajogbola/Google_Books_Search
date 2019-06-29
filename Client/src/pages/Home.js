bb89d714ce93fd18edccfcdf81ce32f76a0523b9
Simmie <skajogbola@users.noreply.github.com>
2019-06-29 07:42:53 -0400

    fixing bugs

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -3,7 +3,6 @@ import Jumbotron from "../components/Jumbotron";
 import Card from "../components/Card";
 import Form from "../components/Form";
 import Book from "../components/Book";
-import Footer from "../components/Footer";
 import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 import { List } from "../components/List";
