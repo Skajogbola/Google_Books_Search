7d5732ec22f4005ac855fe9a5314e02d9916e0cf
Simmie <skajogbola@users.noreply.github.com>
2019-06-29 07:12:33 -0400

    Updating the models folder

--- models/book.js
+++ models/book.js
@@ -4,8 +4,11 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  subtitle: { type: String },
+  link: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  googleId: { type: String, required: true, unique: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
